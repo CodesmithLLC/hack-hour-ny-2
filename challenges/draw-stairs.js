360388c4f23014b4ea7c693548b6cea3455191ac
Eduardo Maíllo Conesa <eduardomaillo@gmail.com>
2018-01-26 09:44:42 -0500

    First commit

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,17 @@
 */
 
 function drawStairs(n) {
-
-}
-
+  let whites = '';
+  for (let i = 1; i <n; i += 1){
+    whites += ' ';
+  }
+    
+  let stars = '*';
+  for (let i = 1; i <=n; i += 1){
+    console.log(whites + stars);
+    whites = whites.substring(0, whites.length -1);
+    stars += '*'
+  }
+};
 
 module.exports = drawStairs;
