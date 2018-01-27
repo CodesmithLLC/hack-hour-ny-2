6ffe7f1ab4c86f5761472c23ae59ba80c4eea1d4
jonathan coe <jonathancoe@jonathans-MacBook-Pro-2.local>
2018-01-27 09:36:19 -0500

    mode-mean first try

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,25 @@
 
 
 function modemean(array) {
+ const mem = {};
+ let total = 0;
+ array.forEach((e)=>{
+ 	total += e;
+ 	if (mem[e]) mem[e]++;
+ 	else mem[e] = 1;
+ })
+ let avg =total/array.length;
+ let numOfRepeats = 0; let mode;
 
+ for (let prop in mem){
+ 	if (mem[prop] > numOfRepeats){
+ 		numOfRepeats = mem[prop];
+ 		mode = Number(prop);
+ 	}
+ }
+ console.log(avg, mode);
+ return avg === mode;
 }
 
+//console.log(modemean([13, 13, 13, 13, 14, 14, 16, 18, 21]));
 module.exports = modemean;
