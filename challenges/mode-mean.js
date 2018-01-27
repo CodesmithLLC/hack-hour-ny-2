3eaeba241510a6459a42d4062849568642363943
WuPenghe <wupenghe@MacBook-Pro-3.local>
2018-01-27 09:31:46 -0500

    mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,22 @@
 
 
 function modemean(array) {
-
+  let sum = 0;
+  const freq = {};
+  for (let num of array) {
+    sum += num;
+    freq.hasOwnProperty(num) ? freq[num]+=1 : freq[num]=0;
+  }
+  const mean = Math.floor(sum/array.length);
+  let mode = array[0];
+  let frequence = 1;   
+  for (let number in freq) {
+    if (freq[number] > frequence && Number(number)>mode) {
+      mode = Number(number);
+      frequence = freq[number];
+    }
+  }
+  return mode === mean;
 }
 
 module.exports = modemean;
