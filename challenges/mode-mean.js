8f374b51ea1bb89e57e3bdd9d1903a99821c6c6a
sarahheacock <seheacock@bellsouth.net>
2018-01-27 09:34:48 -0500

    modemean first attempt

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,33 @@
 
 
 function modemean(array) {
+  let modeObj = {};
+  let mode = array[0];
+  let sum = 0;
 
+  array.forEach((num, i) => {
+    sum += num;
+
+    // keep track of number occurences
+    if(modeObj[num]){
+      modeObj[num] += 1;
+    }
+    else {
+      modeObj[num] = 1;
+    }
+
+    // update mode
+    if(modeObj[num] > modeObj[mode]){
+      mode = num;
+    }
+    else if(modeObj[num] === modeObj[mode]){
+      mode = Math.max(mode, num);
+    }
+  });
+
+  return Math.floor(sum / array.length) === mode;
 }
+
+modemean([1, 2, 3, 3, 4, 5, 5])
 
 module.exports = modemean;
