d4eb466f891cce629aeb7724c2ec17d98c8b5507
Albert <albert.chen@nyu.edu>
2018-01-30 10:19:14 -0500

    kth to last node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,25 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  let count = 1;
+  let temp = head
+  let obj = {};
+  while(temp.next !== null) {
+    obj[count] = temp.value;
+    count++;
+    temp = temp.next;
+  }
 
+  obj[count] = temp.value;
+  let fromLast = k - 1;
+  let positionOfK = count - fromLast;
+   if (count < k || obj[positionOfK]) {
+    return undefined;
+  } else {
+    return obj[positionOfK];
+  }
 }
+
+return temp.value;
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
