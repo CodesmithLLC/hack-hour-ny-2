210e791be0ec1a6087feb4625bd45b74e89ea450
jonathan coe <jonathancoe@jonathans-MacBook-Pro-2.local>
2018-02-02 09:38:21 -0500

    reverse first try

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -12,9 +12,14 @@
  *
  * DO NOT USE THE BUILT IN REVERSE METHOD
  */
-
+let list = [10,20,30,40,50,60,70,80];
 function reverseInPlace(array) {
-
+	let len = array.length-1;
+	for (let i = 0; i < array.length/2; i++){
+		array[i] = array.splice(len, 1, array[i])[0];
+		len--;
+	}
+	return array;
 }
-
+ console.log(reverseInPlace(list));
 module.exports = reverseInPlace;
