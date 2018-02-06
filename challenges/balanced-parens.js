712b10fb1f79d83cdfa1497fe8162ce6935a9a9e
Eduardo Maíllo Conesa <eduardomaillo@gmail.com>
2018-02-06 09:58:10 -0500

    Not finished and wrong approach

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,19 @@
  *
  */
 
-function balancedParens(input){
-
+for (let i = 0; i < input.length; i += 1){
+  let i2 = input.length - 1;
+  if (openBraces.indexOf(input[i]) > 0){
+    let ob = openBraces.indexOf(input[i]);
+    for (i2; i2 > i; i -= 1){
+      if (closeBraces.indexOf(input[i2])){
+        if (closeBraces.indexOf(input[i2]) === ob) { break}
+        else {return false}
+      }
+    }
+  }
+}
+return true
 }
 
 module.exports = balancedParens;
