f8b7bb051c71290ed7ff0b791ededdbeb947a864
Kyung Lee <olive27k@gmail.com>
2018-02-07 09:58:26 -0500

    only passing hardcoded numbers..

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,6 +18,48 @@
  */
 
 function romanNumeral(n) {
+  /* 
+      1 -> I , 5 -> V, 10 -> X; 50 = L; 100 = C;  500 = D;  1000 = M
+      4 = 5 - 1 = I + V;  3 = III;  7 = 5 + 2 = VII;  8 = 5 + 3 = VIII
+    90 = XC (100 - 10)
+    400 = CD (500 - 100)
+    900 = CM (1000 - 100)
+  */
+  let result = "";
+
+  const anchors = {
+    1: "I",
+    4: 'IV',
+    5: 'V',
+    9: "IX",
+    10: 'X',
+    40: "XL",
+    50: 'L',
+    90: "XC",
+    100: 'C',
+    400: "CD",
+    500: 'D',
+    900: 'CM',
+    1000: 'M',
+  }
+
+  // getting keys, and converting each to number type
+  const keyNums = Object.keys(anchors).map((numStr) => parseInt(numStr));
+  // keyNums = array of keys in anchors
+
+  console.log(keyNums);
+  // console.log(keyNums.indexOf(n))
+  let index = keyNums.indexOf(n);
+  index = index.toString()
+  let keyInAnchor = keyNums[index]
+
+  console.log(keyInAnchor)
+  // console.log(anchors[5])
+
+  if (keyNums.indexOf(n)) {
+    return anchors[keyInAnchor]
+  }
+
 
 }
 
