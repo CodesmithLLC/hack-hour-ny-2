01ab5fe689cfdadc1cc8e80445aa57afdc4758b0
ralph salazar <ralph.slzr@gmail.com>
2018-02-10 09:43:28 -0500

    binary attempt 1

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -12,9 +12,30 @@
  * Extension:
  * Write a function that converts a decimal number to binary (then maybe hexadecimal)
  */
+// [1, 2, 4, 6, 8]
+
+function buildEvensArr(str) {
+  let arr = [1];
+  let num = 0;
+  for (let i = 1; i < str.length; i++) {
+    arr.push(num+=2);
+  }
+  return arr.reverse();
+}
 
 function binToDec(binary) {
-
+  if (binary === 0) {
+    return 0;
+  }
+  let binArr = binary.split('');
+  let placesArr = buildEvensArr(binary);
+  let dec = 0;
+  for (let i = binArr.length-1; i >= 0; i--) {
+    if (binArr[i] === '1') {
+      dec += placesArr[i];
+    }
+  }
+  return dec;
 }
 
 module.exports = binToDec;
