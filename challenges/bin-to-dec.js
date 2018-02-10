9f29930d72680948b0d5f7d4d8d4f8393e0f98d0
Kyung Lee <olive27k@gmail.com>
2018-02-10 09:46:10 -0500

    not extension

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,6 +14,35 @@
  */
 
 function binToDec(binary) {
+  /* 2^0 * n, 2*1 * n, 2*2 * n, 2*3 * n, ...
+
+   binary % 10 = first 
+    binary % 100 
+
+    binary.toString().split()
+  */
+  let num = binary.toString().split('');
+  let len = num.length;
+
+  //   console.log(num)
+  //   console.log(parseInt(num[len-1]))
+
+  num = num.map((el) => parseInt(el))
+  //              {
+  //     return parseInt(el)
+  //   });
+  //   console.log(num[len-1])
+
+  let result = 0;
+
+  for (let i = 0; i < len; i++) {
+    result += num[len - i - 1] * Math.pow(2, i);
+  }
+
+
+  console.log(result)
+
+  return result;
 
 }
 
