fa1bce6df88e563f3ef5f95163845586896a94af
jonathan coe <jonathancoe@jonathans-MacBook-Pro-2.local>
2018-02-10 08:01:50 -0500

    subsetsum

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -9,7 +9,26 @@
  */
 
 function subsetSum(array, target) {
+	const combinations = [];
+  const recur = (comb, arr) => {
+   // console.log(comb);
+    for (let i = 0; i < arr.length; i++){
 
+      combinations.push( comb.concat(arr[i]) );
+      
+      recur( comb.concat(arr[i]), arr.slice(i+1) );
+    }
+  }
+	recur([], array);
+  console.log(combinations);
+  let sum;
+  let result = false;
+  combinations.forEach((comb) => {
+    sum = comb.reduce((acc, cur) => acc + cur)
+    //console.log(sum , target);
+    if (sum === target) result = true;
+  })
+  return result;
 }
-
+console.log(subsetSum([3, 34, 4], 7));
 module.exports = subsetSum;
