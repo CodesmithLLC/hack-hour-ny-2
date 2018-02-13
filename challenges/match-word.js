994f2b70b2106b0fc38d2ae73ad6e5e6e802b0ac
angelascerbo <amscerbo@gmail.com>
2018-02-13 09:48:04 -0500

    match word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -11,7 +11,46 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+	// empty string returns true
+	if(!str) return true
+	let firstWord = '', lastWord = '', fristLength;
 
+	// iterate through the string until we get to the first letter (break for loop)
+	for (let i = 0; i < str.length; i++) {
+		// stop iteration and storing letters when we hit a space or special char
+		if(firstWord && str[i].match(/[^A-Za-z]/)) {
+			break;
+		}
+		// store letters to create the first word
+		if(str[i].match(/[A-Za-z]/)) {
+			firstWord += str[i];
+		}
+	}
+
+	firstWord = firstWord.toLowerCase();
+	fristLength = firstWord.length;
+
+	if (str[str.length - 1].match(/[^A-Za-z]/)) {
+		// iterate backwards through the str
+		for (let j = str.length -1; j >= fristLength; j--) {
+			if(lastWord && str[j - 1].match(/[^A-Za-z]/)) {
+				lastWord = str[j] + lastWord.slice(0);
+				break;
+			}
+
+			if(str[j].match(/[A-Za-z]/)) {
+				lastWord = str[j] + lastWord.slice(0);
+			}
+		}
+	} else {
+		// take the length of the first word and slice the string to get the last word
+		lastWord = str.slice(-fristLength);
+	}
+
+	lastWord = lastWord.toLowerCase();
+
+	// compare the first and last words to confirm they're reverse
+	return firstWord.split('').reverse().join('') === lastWord;
 }
 
 module.exports = matchWord;
