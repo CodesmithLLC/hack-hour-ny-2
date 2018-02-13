33d94bd050ab64c746cfbee0bce5bb41f7862f5e
Eduardo Maíllo Conesa <eduardomaillo@gmail.com>
2018-02-13 10:47:44 -0500

    First commit

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -10,8 +10,37 @@
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
-function matchWord(str) {
+function reverseString(str) { 
+  let reversed = '';
+ for (let x = str.length - 1; x >= 0; x -= 1) {
+  reversed += str[x];
+ }
+  return reversed;
+}
 
+
+function matchWord(str) {
+  let lowerStr = str.toLowerCase()
+  let words = [];
+  for (let i = 0; i < lowerStr.length; i += 1) {
+    let word = '';
+    while (i < lowerStr.length && lowerStr[i].match(/[a-z]/)) {
+      word += lowerStr[i];
+      i += 1
+    }
+    if (word.length !== 0) {words.push(word)}
+  }
+  for (let i = 0; i < Math.floor(words.length); i += 1) {
+    for (let j = i + 1; j < words.length; j += 1){
+      if (words[i] === reverseString(words[j])) {
+        if ((j === i + 1 || (j-i +1) % 2 === 0) ) {
+          continue
+        }
+        else {return false}
+      }
+    }
+  }
+  return true
 }
 
 module.exports = matchWord;
