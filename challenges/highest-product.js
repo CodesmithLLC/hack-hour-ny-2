aecac5fba4e31989b1d7248d7ff0473c67fb59c9
ralph salazar <ralph.slzr@gmail.com>
2018-02-14 09:29:30 -0500

    highest product attempt 1

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,8 +2,20 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
+function sortArr(array) {
+  return array.sort((a,b) {
+    return b - a;
+  });
+}
 
+function highestProduct(array) {
+  // sort array
+  // find the product of the biggest numbers
+  let sorted = sortArr(array);
+  let largest = sorted.slice(0, 3);
+  return largest.reduce((acc, curr) => {
+    return acc * curr;
+  });
 }
 
 
