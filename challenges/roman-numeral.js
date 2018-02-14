bdf138c8879287bc131230335872a40d91312bf3
Kyung Lee <olive27k@gmail.com>
2018-02-14 09:43:19 -0500

    highest product

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -60,23 +60,54 @@ function romanNumeral(n) {
   //   return anchors[keyInAnchor]
   // }
 
-  const anchors = [
-    1000, 'M',
-    900, 'CM',
-    500, 'D',
-    400, "CD",
-    100, 'C',
-    90, "XC",
-    50, 'L',
-    40, "XL",
-    10, 'X',
-    9, "IX",
-    5, 'V',
-    4, 'IV',
-    1, "I",
-  ] 
-
   
+  // *** method #2 ***
+  // starting from making an array
+  // const anchors = [
+  //   1000, 'M',
+  //   900, 'CM',
+  //   500, 'D',
+  //   400, "CD",
+  //   100, 'C',
+  //   90, "XC",
+  //   50, 'L',
+  //   40, "XL",
+  //   10, 'X',
+  //   9, "IX",
+  //   5, 'V',
+  //   4, 'IV',
+  //   1, "I",
+  // ] 
+
+  // for(let i )
+
+
+  // *** method #3 ***
+  let numerals = {
+    '1': 'I',
+    '4': 'IV',
+    '5': 'V',
+    '9': 'IX',
+    '10': 'X',
+    '40': 'XL',
+    '50': 'L',
+    '90': 'XC',
+    '100': 'C',
+    '400': 'CD',
+    '500': 'D',
+    '900': 'CM',
+    '1000': 'M'
+  }
+
+  if (numerals[n]) return numerals[n];
+  let nums = Object.keys(numerals).reverse();
+  for (var i = 0; i <= nums.length; i++) {
+    while (n % nums[i] < n) {
+      result += numerals[nums[i]];
+      n -= nums[i];
+    }
+  }
+  return result
 
 }
 
