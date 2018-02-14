88325c9293a48337fc231aa810df27a6a927ff85
Eduardo Maíllo Conesa <eduardomaillo@gmail.com>
2018-02-14 09:35:03 -0500

    3rd commit

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -18,9 +18,11 @@ function sortArray(array) {
 
 
 function highestProduct(array) {
+  if (array.length < 3 || !Array.isArray(array)) return 0;
   const sorted = sortArray(array);
   let product = 1;
-  for (i = sorted.length - 1; (i >= sorted.length - 3 && i >= 0); i -= 1) {
+  for (i = sorted.length - 1; i >= sorted.length - 3; i -= 1) {
+    console.log(sorted[i])
     product *= sorted[i]
   }
   return product
