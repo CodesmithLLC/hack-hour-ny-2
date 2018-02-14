280c2f265bb33b43edbc6c48003d4b5256aa73f4
jonathan coe <jonathancoe@jonathans-MacBook-Pro-2.local>
2018-02-14 09:51:37 -0500

    highest prod

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,32 @@
  */
 
 function highestProduct(array) {
+	const combinations = [];
+  const recur = (comb, arr) => {
+   // console.log(comb);
+    for (let i = 0; i < arr.length; i++){
+    	if (comb.length === 2)
+      	combinations.push( comb.concat(arr[i]) );
+      
+      recur( comb.concat(arr[i]), arr.slice(i+1) );
+    }
+  }
+	recur([], array);
+  
+  let result = false;
+  let product;
+  let greatest = combinations[0].reduce((acc, cur) => 
+  	acc * cur, 
+  	combinations[0][0])
 
+  
+  combinations.forEach((comb) => {
+    product = comb.reduce((acc, cur) => 
+    	acc * cur)
+    if (product > greatest) greatest = product;
+  })
+  return greatest;
 }
 
-
+console.log(highestProduct([1, 2, 3, 4]));
 module.exports = highestProduct;
