3307dd36dd8dcd5bb69535e589aebd673e9477c5
Eduardo Maíllo Conesa <eduardomaillo@gmail.com>
2018-02-14 09:26:27 -0500

    First commit

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,8 +2,28 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
+function sortArray(array) {     
+  for (var i = 0; i < array.length; i += 1) {
+    for (var j = i + 1; j < array.length; j += 1) {
+      if (array[j] < array[i]) {
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+      }
+    }
+  }
+  return array;
+}
 
+
+
+function highestProduct(array) {
+  const sorted = sortArray(array);
+  let product = 1;
+  for (i = sorted.length - 1; (i >= sorted.length - 3 || i === 0); i -= 1) {
+    product *= sorted[i]
+  }
+  return product
 }
 
 
