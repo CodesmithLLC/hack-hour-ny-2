459aa72d78eb42e09a29a932f6c17da7017085c3
jmw1493 <weissjmw@gmail.com>
2018-02-14 09:54:29 -0500

    accounts for negatives

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,11 +3,32 @@
  */
 
 function highestProduct(array) {
-  if (!array || array.length < 3) return false;
+  if (!array || array.length < 3) return 0;
+  let newArr = array.sort(function(a, b) {
+    return b - a;
+  });
+  let negProduct = 1;
+  let posProduct = 1;
+
+  for (let i = newArr.length - 2; i >= 0; i--) {
+    if (newArr[i] < 0 && newArr[i + 1] < 0) {
+      negProduct *= newArr[i] * newArr[i + 1];
+      break;
+    }
+  }
+  if (negProduct > 1) negProduct *= newArr[0];
+  posProduct *= array[0] * array[1] * array[2]
+  return Math.max(negProduct, posProduct);
+}
+
+
+
+
+function highestProduct(array) {
+  if (!array || array.length < 3) return 0;
   let newArr = array.sort(function(a, b){
     return b - a;
   });
-  let total = newArr[0] * newArr[1] * newArr[2];
   return total;
 }
 
