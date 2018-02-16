dd5e087f603d1e8e80a282aaa139b6f9d3a05928
sarahheacock <seheacock@bellsouth.net>
2018-02-16 09:40:38 -0500

    first attempt

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,44 @@ function Node(val) {
   this.next = null;
 }
 
+// function create(arr){
+//   let prev = null;
+//   let current = null;
+//   let start = null;
+//
+//   arr.forEach((val, i) => {
+//     prev = current;
+//     current = new Node(val);
+//     if(prev){
+//       prev.next = current;
+//     }
+//     else {
+//       start = current;
+//     }
+//   });
+//   return start;
+// }
+
 function zip(l1, l2) {
+  let currentOne = l1;
+  let currentTwo = l2;
+
+  while(currentOne !== null && currentTwo !== null){
+    let tempOne = currentOne.next;
+    currentOne.next = new Node(currentTwo.value);
+    currentOne.next.next = tempOne;
+
+    currentOne = tempOne;
+    currentTwo = currentTwo.next;
+  }
+
+  return l1;
 };
+
+// const listOne = create([0, 2, 4]);
+// const listTwo = create([1, 3, 5]);
+//
+// zip(listOne, listTwo);
+// console.log(JSON.stringify(listOne, null, 4))
 
 module.exports = {Node: Node, zip: zip};
