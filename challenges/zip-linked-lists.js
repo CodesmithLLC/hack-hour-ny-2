e1f0f5fcc383215ca07d54d8f2af3f4b93959770
FrankJuH <frank@preferredreturn.com>
2018-02-16 09:35:10 -0500

    Testing

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -5,22 +5,60 @@
  * BONUS: Do this in place
  */
 
+
+
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
 function zip(l1, l2) {
-	let newObj = new Node(l1)
-	let newObj2 = new Node (l2)
-	newObj.next = newObj2; 
+
+	let arrayValue1 = [];  
+	let arrayValue2 = []; 
+	let mergedValueArr = [];
+	let newObj;  
 
 
+	if(arrayValue1.length > arrayValue2.length)
+		let length = arrayValue1.length;
+	else
+		let length = arrayValue2.length; 
 
-// testing to see what the test cases are. 
+	function linkedList(node){
+		if(this.next === null)
+			return; 
+		else
+			let value = this.value; 
+			arrayValue1.push(value);
+			linkedList(this.next); 
+	}
+
+	linkedList(l1);
+
+		function linkedList2(node){
+		if(this.next === null)
+			return; 
+		else
+			let value = this.value; 
+			arrayValue2.push(value);
+			linkedList2(this.next); 
+	}
+
+	linkedList(l2);
+
+	for(let i = 0; i < length; i++){
+		mergedValueArr.push(arrayValue1[i]);
+		mergedValueArr.push(arrayValue2[i]);
+	}
+
+	for(let i = 0; i < mergedValueArr.length; i++){
+		if(newObj)
+			newObj.next = new Node(mergedValueArr[i])
+		newObj = new Node(mergedValue[i]);
+	}
+	
 	return newObj; 
-
-
 };
 
 module.exports = {Node: Node, zip: zip};
