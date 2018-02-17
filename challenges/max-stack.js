63330bef9ddd6207902d0f2f344e77b92de93c83
Eduardo Maíllo Conesa <eduardomaillo@gmail.com>
2018-02-17 09:39:05 -0500

    First commit

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,8 +6,32 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-function Stack() {
-  // body...
+function Stack(val) {
+  this.storage = {};
+  this.length = 0;
+}
+
+stack.prototype.push = function(val) {
+  this.storage[this.length] = val;
+  this.length += 1;
+  return this.length;
+}
+
+stack.prototype.pop = function() {
+  const output = this.storage[this.length - 1];
+  delete this.storage[this.length - 1];
+  this.length -= 1;
+  return output;
+}
+
+stack.prototype.getMax = function() {
+  let max = Number.NEGATIVE_INFINITY;
+  for (let key in this.storage) {
+    if (this.storage[key] > max) {
+      max = this.storage[key];
+    }
+  }
+  return max;
 }
 
 module.exports = Stack;
