852188568dff3c204e9b2daebd17883e7b7046ce
WuPenghe <wupenghe@MacBook-Pro-3.local>
2018-02-17 09:28:16 -0500

    stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,8 +6,32 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-function Stack() {
-  // body...
-}
+ function Stack() {
+   this.content = [];
+   this.length = 0;
+   this.max = null;
+ }
 
+ Stack.prototype.push = function(item) {
+   this.content[this.length] = item;
+   this.length++;
+   if (this.max === null || this.max < item) {
+     this.max = item;
+   }
+   return this.length;
+ }
+
+ Stack.prototype.pop = function() {
+   if (this.length === 0) return undefined;
+   const last = this.content.pop();
+   //this.content[this.length-1] = undefined;
+   this.length--;
+   if (this.length === 0) this.max = null;
+   else this.max = Math.max(...this.content);
+   return last;
+ }
+
+ Stack.prototype.getMax = function() {
+   return this.max;
+ }
 module.exports = Stack;
