c70956d7b5917834cba5457b470822d3ffb55950
sartra <strenshaw@gmail.com>
2018-02-20 10:26:04 -0500

    rev linked list

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -14,53 +14,78 @@
   * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
   */
 
- function Stack() {
-   this.contents = {};
-   this.length = 0;
-   this.max = Stack[0];
- }
+  /*
+   * Create a stack with the push, pop, and getMax methods.
+   * push should return the new length of the stack.
+   * pop should return the element that was just removed.
+   * getMax should return the largest value currently in the stack.
+   * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
+  */
 
- Stack.prototype.push = function (val) {
-     // Place the new value into the
-     // next available key (index.length)
-     this.contents[this.length] = val;
+// Linear time
 
-     // Update the length property
-     this.length++;
+  function LStack() {
+    this.stack = [];
+    this.length = 0;
+  }
 
-     // if this is first val, set this to be max
-     if (this.length === 1) {
-       this.max = val;
-     }
-     // reset max if this val is greater than max
-     if (val > this.max) {
-       this.max = val;
-     }
-     // Return the updated length
-     return this.length;
- }
+  LStack.prototype.push = (val) => {
+    this.stack[this.length] = val;
+    return ++this.length;
+  }
 
- Stack.prototype.pop = function (val) {
-   let index = this.length;
-   let output = this.contents[index-1];
-   delete this.contents[index-1];
-   this.length--;
-   return output;
- }
+  LStack.prototype.pop = () => {
+    if (this.length === 0) return undefined;
+    let temp = this.stack[--this.length];
+    delete this.stack[this.length];
+    return temp;
+  }
 
- Stack.prototype.getMax = function () {
-   return this.max;
- }
+  LStack.prototype.getMax = () => {
+    if (this.length === 0) return undefined;
+    let max = null;
+    this.stack.forEach((num) => {
+      if (max === null || max < num) max = num;
+    });
+    return max;
+  }
 
 
- var stack = new Stack;
 
- stack.push(2)
- stack.push(6)
- stack.push(3)
- stack.push(9)
- stack.pop()
- console.log(stack)  // ERROR: Max still remains even after value popped. How to fix???
+////////////////////////////////////////////////
+  /** Now let's do it in constant time **/
+
+  function CStack() {
+    this.stack = [];
+   	this.maxStack = [];
+  }
+
+  CStack.prototype.push = function(value) {
+  	this.stack.push(value);
+  	// if maxStack is empty or if the value is greater than the max so far,
+  	// let's push the value, otherwise, our last max is still our max at the new index
+  	let max = (!this.maxStack.length || value > this.maxStack[this.maxStack.length - 1]) ? value : this.maxStack[this.maxStack.length - 1];
+
+  	return this.maxStack.push(max);
+  }
+
+  CStack.prototype.pop = function() {
+  	this.maxStack.pop();
+  	return this.stack.pop();
+  }
+
+  CStack.prototype.getMax = function() {
+  	return this.maxStack[this.maxStack.length - 1];
+  }
+
+  let newStack = new CStack();
+  newStack.push(6)
+  newStack.push(10)
+  newStack.push(12)
+  newStack.push(8)
+  newStack.push(7)
+  newStack.getMax();
+
 
 
 module.exports = Stack;
--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -1,3 +1,38 @@
+// create LL for testing
+
+function LinkedList(){
+  this.head = null;
+}
+
+LinkedList.prototype.push = function(val){
+ let node = {
+   value: val,
+   next: null
+ }
+ if(!this.head){
+   this.head = node;
+ } else {
+   current = this.head;
+   while(current.next){
+     current = current.next;
+   }
+   current.next = node;
+ }
+}
+
+let ll = new LinkedList();
+ll.push(2)
+ll.push(3)
+ll.push(4)
+ll.push(5)
+
+console.log(ll.head) // 2
+console.log(ll.head.next) // 3
+console.log(ll.head.next.next) // 4
+
+console.log('Forward:')
+console.log(ll)
+
 /**
  * Write a function for reversing a linked list.
  * Your function will have one input: the head of the list
@@ -7,14 +42,41 @@
  * Do it in place
  *
  */
-
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = val;
+  this.next = null;
 }
 
 function reverseLinkedList(head) {
+ if(!head && !head.next) return head;
 
+ const nodes = [];
+ let current = head;
+
+ // store all nodes in an Array
+ while(current){
+   nodes.push(current);
+   current = current.next;
+ }
+
+ let reversedLL = new LinkedList();
+
+ reversedLL.head = nodes.pop();
+ current = reversedLL.head;
+
+ let node = nodes.pop();
+
+ //make sure to make next of the newly inserted node to be null
+  //other wise the last node of your SLL will retain its old next.
+  while(node){
+    node.next = null;
+    current.next = node;
+
+    current = current.next;
+    node = nodes.pop();
+  }
+  return reversedLL;
 }
 
-module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
+console.log('Reversed: ')
+reverseLinkedList(ll.head)
