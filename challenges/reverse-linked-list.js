ffbfa1af45084f79bb0d07badff437e7d133981f
sarahheacock <seheacock@bellsouth.net>
2018-02-20 09:37:12 -0500

    reverse LL

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,45 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
 
+  if(!current.next){
+    return head;
+  }
+
+  while(current.next){
+    let temp = current.next;
+    current.next = prev;
+
+    prev = current;
+    current = temp;
+  }
+
+  current.next = prev;
+  return current;
 }
+
+function create(arr){
+  let prev = null;
+  let current = null;
+  let start = null;
+
+  arr.forEach((val, i) => {
+    prev = current;
+    current = new Node(val);
+    if(prev){
+      prev.next = current;
+    }
+    else {
+      start = current;
+    }
+  });
+  return start;
+}
+
+let list = create([1, 2, 3, 4, 5]);
+console.log(JSON.stringify(list, null, 4));
+console.log(JSON.stringify(reverseLinkedList(list), null, 4));
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
