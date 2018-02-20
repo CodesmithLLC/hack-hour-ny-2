2fb5f29a658d70e59d618a956478262d0fc49fad
Marissa L <marissal.io@gmail.com>
2018-02-20 09:32:50 -0500

    Attempt #1 RLL

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -1,4 +1,4 @@
-/**
+   /**
  * Write a function for reversing a linked list.
  * Your function will have one input: the head of the list
  * Your function should return the new head of the list
@@ -14,7 +14,22 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+    if(!head) return null;
+    if(!head.next) return head;
+    let previous = head;
+    let current = previous.next;
+    previous.next = null;
 
+    while(current.next !== null) {
+        let nextVal = current.next;
+        current.next = previous;
+        previous = current;
+        current = nextVal;
+    }
+
+    current.next = previous;
+
+    return current;
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
