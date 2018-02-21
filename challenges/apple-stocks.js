94c4f71e03031e5d78b8204425d6e687043148b5
Eduardo Maíllo Conesa <eduardomaillo@gmail.com>
2018-02-21 09:33:14 -0500

    First Commit - needs refactoring

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -12,8 +12,20 @@
  *  Return 0 if no profit is possible OR if input is invalid.
  */
 
-function bestProfit(stock_prices_yesterday) {
-
+function bestProfit(s_p_y) {
+  let absoluteMax = 0;
+  for (let i = 0; i < s_p_y.length; i += 1) {
+    let tempMax = 0;
+    for (let j = i; j < s_p_y.length; j += 1) {
+      if (s_p_y[j] - s_p_y[i] > tempMax) { 
+        tempMax = s_p_y[j] - s_p_y[i]
+      }
+    }
+    if (tempMax > absoluteMax) {
+      absoluteMax = tempMax;
+    }
+  }
+  return absoluteMax;
 }
 
 module.exports = bestProfit;
