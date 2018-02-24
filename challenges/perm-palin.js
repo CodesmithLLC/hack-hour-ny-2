92b72c4ad9a385423e69e63cd2489b33ce8c456e
Kyung Lee <olive27k@gmail.com>
2018-02-24 11:07:13 -0500

    perpalin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,8 +9,35 @@
  *
  */
 
+
 function permPalin(str) {
-	
+  if (str.length === 1) return true;
+
+  let result;
+  let arr = str.split('');
+  let hash = {};
+  // console.log(arr)
+
+  for (let item of arr) {
+
+    if (hash[item]) {
+      hash[item] += 1;
+    } else {
+      hash[item] = 1;
+    }
+
+  }
+  // console.log(hash)
+  let odd = 0;
+  for (let char in hash) {
+    // console.log(char)
+    if (hash[char] % 2 !== 0) {
+      odd += 1;
+    }
+    result = odd > 1 ? false : true;
+  }
+  return result;
+
 }
 
 module.exports = permPalin;
