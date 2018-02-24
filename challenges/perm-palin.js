d8fd8017ffdac3a0579dca9c8dbd1446a68741b0
WuPenghe <wupenghe@MacBook-Pro-3.local>
2018-02-24 09:22:47 -0500

    perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,22 @@
  */
 
 function permPalin(str) {
-	
+	const count = {};
+	let l = str.length;
+	for (let i=0; i<l; i++) {
+		if(!count[str[i]]) {
+			count[str[i]] = 1;
+		} else {
+			count[str[i]]++;
+		}
+	}
+	let check = 0;
+	for (let letter in count) {
+		if (count[letter] % 2 === 1) check++;
+		if (check > 0 && l % 2 === 0 ) return false;
+		if (check > 1 && l % 2 === 1)	return false;
+	}
+	return true;
 }
 
 module.exports = permPalin;
