724ec7d0e4322bba2b6a3f10aa2c2569dbfb32e1
sartra <strenshaw@gmail.com>
2018-02-24 09:43:29 -0500

    perm palindrom

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -16,14 +16,14 @@
 
 
 permPalin = (str) => {
-  
+
   const len = str.length; 
   if(len === 1) return true;
   const hash = {}; 
   
   
   for(var char of str){
-    
+
     if(!hash[char]) hash[char] = 1;
     else hash[char] += 1;
     
@@ -31,20 +31,20 @@ permPalin = (str) => {
   
   let vals = Object.values(hash);
   console.log(vals)
-  
-    // if even - there needs to be even count of all letters 
+
+    // if even len str - there needs to be even count of all letters 
     if(len % 2 === 0){
       return vals.reduce((curr, next) => {
         return curr && isEven(next)
       }, true)
     }
 
-    // if odd length, all chars but one need even count
+    // if odd length str, all chars but one need even count
     if(len % 2 !== 0){
      let output = vals.filter((val) => (!isEven(val)))
      return output.length === 1; 
    }
-   
+
  }
 
 
