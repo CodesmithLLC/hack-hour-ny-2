317d2ba72a3cb9e240c61669a506c313251969c2
jonathan coe <jonathancoe@jonathans-MacBook-Pro-2.local>
2018-02-24 09:49:14 -0500

    perm palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,8 +9,48 @@
  *
  */
 
+ const isPalin = (arr) => {
+ 	for (let i = 0; i < arr.length/2; i++){
+ 		if (arr[i] !== arr[arr.length-1-i]) return false
+ 	}
+ return true;
+ }
+
 function permPalin(str) {
-	
+
+	let results = [];
+	let bool = false;
+
+  if (str.length === 1) 
+    return true;
+
+  const recur = (arr , perm = []) =>{
+  	if (arr.length === 0){
+  		results.push(perm);
+  	}
+  	else{
+  		for (let i = 0; i < arr.length; i++){
+  			let curr = arr.slice();
+  			let next = curr.splice(i, 1);
+  			recur (curr.slice(), perm.concat(next))
+  		}
+  	}
+  }
+  recur(str.split(''))
+
+  console.log(results)
+
+  results.forEach((perm) => {
+  	if (isPalin(perm)) bool = true;
+  })
+  
+  return bool;
 }
 
+console.log(permPalin('cbac'))
+
 module.exports = permPalin;
+
+
+
+  
