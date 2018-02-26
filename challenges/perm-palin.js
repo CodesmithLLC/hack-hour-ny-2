cfbb6ec76e2e38a55c8aa3b62beb048903e5eec3
Albert <albert.chen@nyu.edu>
2018-02-26 09:48:58 -0500

    hfkfh

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -8,9 +8,31 @@
  * 	- permPalin('a') => true
  *
  */
+// if even then make sure each letter has a copy
+// if odd then make sure that there is only one letter that doesn't have a copy
+
+
 
 function permPalin(str) {
-	
+	let letters = str.split('');
+	if (letters.length === 1) {
+		return digits;
+	}
+	const allPermutations = [];
+	for (let i = 0; i < letters.length; i++) {
+		const lettersCopy = letters.slice(0);
+		const fixedLetter = lettersCopy.splice(i, 1);
+		const recursivePermutations = permute(lettersCopy);
+		for (let j = 0; j < recursivePermutations.length; j++) {
+			const recursivePermutation = recursivePermutations[j];
+			allPermutations.push(fixedDigit.concat(recursivePermutation));
+		}
+	}
+	return allPermutations;
+}
+
+function isPalindrome() {
+
 }
 
 module.exports = permPalin;
