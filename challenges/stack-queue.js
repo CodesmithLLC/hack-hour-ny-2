85ec6fb25c31f1bca3eb4b72f2069507c48f5565
Albert <albert.chen@nyu.edu>
2018-02-28 09:20:56 -0500

    fkjhgk

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,9 +4,25 @@
 
 
 function Stack() {
-
+  this.storage = {},
+  this.index = 0
 }
 
+Stack.prototype.push = function(val) {
+  this.storage[this.index] = val;
+  this.index++;
+}
+
+Stack.prototype.pop = function() {
+  if (this.index === 0) {
+    return undefined;
+  }
+  let lastVal = this.storage[this.index - 1];
+  if (this.index > 0) {
+    this.index--;
+  }
+  return lastVal;
+}
 
 /**
 * Queue Class
@@ -14,7 +30,25 @@ function Stack() {
 
 
 function Queue() {
-
+  this.storage = {},
+  this.index = 0
 }
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.index] = 0,
+  this.index++
+}
+
+Queue.prototype.dequeue = function(value) {
+  if (this.index === 0) {
+    return undefined
+  }
+  let firstVal = this.storage[0];
+  delete this.storage[0];
+  this.index--;
+  for (let i = 0; i < this.index; i++) {
+    this.storage[i] = this.storage[i+1];
+    delete this.storage[i+1]
+  }
+}
 module.exports = {Stack: Stack, Queue: Queue};
