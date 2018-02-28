e103fb391208bf3785042be264022d3f9a35a25c
Marissa L <marissal.io@gmail.com>
2018-02-28 09:30:17 -0500

    Attempt #1 two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,8 +2,11 @@
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
 
+function twoSum(arr, n, count = 0) {
+    if (!arr.length) return false;
+    if (!n) return count === 2 ? true : false;
+    return twoSum(arr.splice(1), n - arr[0], count + 1) || twoSum(arr.splice(1), n, count);
 }
 
 module.exports = twoSum;
