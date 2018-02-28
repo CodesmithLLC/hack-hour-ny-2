088dad8be094a6688ac7555d93f9ee63359920e6
angelascerbo <amscerbo@gmail.com>
2018-02-28 09:33:23 -0500

    two sum and rotate grid

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -17,7 +17,21 @@
  */
 
 function rotateGrid(grid, n) {
+	// inputs: 2D arrays (arrays nested within an array), n ?? 
+	// rotate in place (same space in memory)
+	for (let j = n - 1; j >= 0; j--) {
+		for (let i = 0; i < n; i++) {
+			grid[i].push(grid[j][i]);
+		}
+	}
 
+	return grid.map((arr) => {
+		return arr.slice(n);
+	})
 }
+
+const sampleGrid = [  [1, 2, 3], [4, 5, 6], [7, 8, 9]  ];
+
+console.log(rotateGrid(sampleGrid, 3));
 
 module.exports = rotateGrid;
--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,24 @@
  */
 
 function twoSum(arr, n) {
+	// inputs: 
+		// array of itegers - consider negatives and zeros
+		// n = target
+	// output: boolean 
 
+	let remainder;
+	// iterate thru nums in array
+	for (let i = 0; i < arr.length - 1; i++) {
+		// a num has to be less than the target to be considered 
+		if (arr[i] < n) {
+			// if less than caculate target minus the num
+			remainder = n - arr[i];
+			// check if array contains the remainder > return true
+			if (arr.includes(remainder)) return true;
+		}
+	}
+
+	return false;
 }
 
 module.exports = twoSum;
