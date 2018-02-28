7eef1d59da502f1d38954c6b07ce26ccf2d45a5c
sartra <strenshaw@gmail.com>
2018-02-28 09:39:46 -0500

    2sum

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,21 +6,6 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
- /*
-  * Create a stack with the push, pop, and getMax methods.
-  * push should return the new length of the stack.
-  * pop should return the element that was just removed.
-  * getMax should return the largest value currently in the stack.
-  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
-  */
-
-  /*
-   * Create a stack with the push, pop, and getMax methods.
-   * push should return the new length of the stack.
-   * pop should return the element that was just removed.
-   * getMax should return the largest value currently in the stack.
-   * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
-  */
 
 // Linear time
 
