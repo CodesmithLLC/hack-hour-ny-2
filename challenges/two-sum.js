2383037ef40a69536cb631dcf40629d71c9587f9
angelascerbo <amscerbo@gmail.com>
2018-02-28 10:06:55 -0500

    two sum again

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -10,17 +10,21 @@ function twoSum(arr, n) {
 
 	let remainder;
 	// iterate thru nums in array
-	for (let i = 0; i < arr.length - 1; i++) {
+	for (let i = 0; i < arr.length; i++) {
 		// a num has to be less than the target to be considered 
 		if (arr[i] < n) {
 			// if less than caculate target minus the num
 			remainder = n - arr[i];
 			// check if array contains the remainder > return true
-			if (arr.includes(remainder)) return true;
+			if (arr.slice(i).includes(remainder)) return true;
 		}
 	}
 
 	return false;
 }
 
+
+console.log(twoSum([1,2,3,6,0], 6)); // -> true
+
 module.exports = twoSum;
+
