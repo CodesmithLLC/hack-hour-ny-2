2f48172dcc0d2d811b27cc65c641e58d88e8546c
sarahheacock <seheacock@bellsouth.net>
2018-03-01 10:14:33 -0500

    first attempt

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -4,7 +4,7 @@
  *      at any given node, the value of all the nodes in its right tree must be > its value
  * Assume that each value in the tree is unique.
  */
- 
+
 
 function BinaryTree(val) {
     this.value = val;
@@ -12,8 +12,55 @@ function BinaryTree(val) {
     this.right = null;
 }
 
-function validBST(tree) {
+// BinaryTree.prototype.insert = function(val){
+//   let current = this;
+//   let direction = (val > current.value) ? 'right': 'left';
+//   let next = current[direction];
+//
+//   while(next !== null){
+//     current = next;
+//     direction = (val > current.value) ? 'right': 'left';
+//     next = current[direction]
+//   }
+//
+//   current[direction] = new BinaryTree(val);
+// }
 
+BinaryTree.prototype.traverse = function(cb) {
+  // left, center, right
+  if(!this){
+    return;
+  }
+
+  if(this.left){
+    this.left.traverse(cb);
+  }
+  cb(this.value);
+  if(this.right){
+    this.right.traverse(cb);
+  }
 }
+
+function validBST(tree) {
+  let arr = [];
+  tree.traverse(function(val){
+    arr.push(val)
+  });
+
+  let prev = arr[0]
+  return arr.slice(1).reduce((bool, val) => {
+    const result = bool && prev <= val;
+    prev = val;
+    return result;
+  }, true);
+}
+
+// let bt = new BinaryTree(10);
+// const ins = [5, 3, 11, 1, 4, 7, 9, 15, 12, 16];
+// ins.forEach((val) => {
+//   bt.insert(val);
+// })
+// console.log(JSON.stringify(bt, null, 4));
+// console.log(validBST(bt));
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
