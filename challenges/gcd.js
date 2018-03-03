30e31c4774c33cf462eab8a5e592b2962547e75a
WuPenghe <wupenghe@MacBook-Pro-3.local>
2018-03-03 09:35:28 -0500

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,14 +7,22 @@
  *
  */
 
+// function gcd(a, b) {
+//   let i = 1;
+//   let result = 1;
+//   while (i<a && i<b) {
+//     i++;
+//     if (a%i===0 && b%i===0) result = i;
+//   }
+//   return result;
+// }
+
 function gcd(a, b) {
-  let i = 1;
-  let result = 1;
-  while (i<a && i<b) {
-    i++;
-    if (a%i===0 && b%i===0) result = i;
-  }
-  return result;
+  if (a%b === 0) return b;
+  if (b%a === 0) return a;
+  let d = (a>b) ? b   : a;
+  let c = (a>b) ? a%b : b%a;
+  return gcd(d, c);
 }
 
 module.exports = gcd;
