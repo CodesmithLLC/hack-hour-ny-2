adce90e4fb6239be42924c66b909b91788f7b97d
Eduardo Maíllo Conesa <eduardomaillo@gmail.com>
2018-03-03 09:23:57 -0500

    First Comit

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,25 @@
  */
 
 function gcd(a, b) {
-
+  let arr = []
+  let brr = []
+  for (let i = 1; i <= a; i += 1){
+    if (a % i === 0) {
+      arr.push(i)
+    }
+  }
+  for (let i = 1; i <= b; i += 1){
+    if (b % i === 0) {
+      brr.push(i)
+    }
+  }
+ console.log(arr)
+ console.log(brr)
+  for (let i = arr.length -1; i >= 0; i -= 1) {
+    if (brr.indexOf(arr[i]) !== -1) {
+      return arr[i]
+    }
+  }
 }
 
 module.exports = gcd;
