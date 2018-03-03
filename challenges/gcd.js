87d12caeff8e9122efb4d67cefb6679f57814667
jonathan coe <jonathancoe@jonathans-MacBook-Pro-2.local>
2018-03-03 09:32:44 -0500

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,19 @@
  */
 
 function gcd(a, b) {
-
+	let divisor = a > b ? b : a;
+	let found = false;
+	while (divisor > 1 && !found){
+		if (a%divisor === 0 && b%divisor === 0)
+			found = true;
+		if (!found) divisor--;
+	}
+	return divisor;
 }
+
+// console.log(gcd(10,8))
+// console.log(gcd(10,9))
+// console.log(gcd(10,5))
+// console.log(gcd(40,100))
 
 module.exports = gcd;
