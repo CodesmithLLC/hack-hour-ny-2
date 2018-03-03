1ac7777679890d9ae9fd4d4b88605ec895f64d15
Kyung Lee <olive27k@gmail.com>
2018-03-03 09:57:11 -0500

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,17 @@
  */
 
 function gcd(a, b) {
+  // divide both numbers with each number and find the greatest number that divides it?
+  let smaller;
+  a > b ? smaller = b : smaller = a;
+  let divisor = 1;
 
+  for(let i = 0; i < smaller; i++) {
+    if (a % i === 0 && b % i === 0 && i > divisor) {
+      divisor = i;
+    }
+  }
+  return divisor;
 }
 
 module.exports = gcd;
