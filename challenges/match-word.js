ce71ed44285be8e63f763cc8544f1071bdeea42a
Albert <albert.chen@nyu.edu>
2018-03-14 09:25:57 -0400

    khgk

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -1,6 +1,9 @@
-// Some languages have "if" statements that are closed by "fi" instead of curly brackets. Or they close a "case" with "esac",
-//i.e. the same keyword backwards. for this problem we'll check that all words in a string are "closed". Write a function that
-//takes a string and returns true if every word is closed by its backwards counterpart. Words must be separated by space or
+// Some languages have "if" statements that are closed by "fi" instead of curly brackets. Or they close a 
+// "case" with "esac",
+//i.e. the same keyword backwards. for this problem we'll check that all words in a string 
+//mare "closed". Write a function that
+//takes a string and returns true if every word is closed by its backwards counterpart.
+// Words must be separated by space or
 //punctuation.
 
 // matchWord('__END_DNE-----');  -> true
