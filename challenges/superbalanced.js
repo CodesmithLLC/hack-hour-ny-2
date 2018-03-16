7a563929ad85c9180ab2996b6c0f402ff758d65e
jonathan coe <jonathancoe@jonathans-MacBook-Pro-2.local>
2018-03-16 09:45:20 -0400

    superbalanced

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -13,8 +13,27 @@ function BinaryTree(value) {
   this.right = null;
 }
 
+const A = new BinaryTree(2);
+A.left = new BinaryTree(1);
+A.left.left = new BinaryTree(0)
+A.right = new BinaryTree(3);
+A.right.right = new BinaryTree(4)
+A.right.left = new BinaryTree(3.5)
+
+const recur = (node) => {
+		if (node === null) return 0;
+		return Math.max(recur(node.left), recur(node.right)) + 1;
+	}
+
 function superbalanced(tree) {
+	if (tree === null) return true;
+
+	let height = recur(tree.left) - recur(tree.right);
+	if (Math.abs(height)>1) return false;
+	else return superbalanced(tree.left) && superbalanced(tree.right);
 
 }
+
+//console.log(superbalanced(A))
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
