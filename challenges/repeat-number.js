ef674dc467e8543922e827491d12d70d40638d34
FrankJuH <frank@preferredreturn.com>
2018-03-16 09:11:45 -0400

    Hack Hour

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,24 @@
  */
 
 function repeatNumbers(array) {
+	let obj = {}; 
+	let holder; 
 
+	for(let i = 0; i < array.length; i++){
+		if(obj[array[i]])
+			obj[array[i]]++
+		else{
+			obj[array[i]] = 1; 
+		} 
+	}
+
+	for(let key in obj{
+		if obj[key] >= 2
+			holder = key
+	})
+
+	return holder; 
+	// not 0(n) or 0(1). Going to go work on our production project...
 }
 
 module.exports = repeatNumbers;
