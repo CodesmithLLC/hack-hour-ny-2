b81c39255777263ec50e976be913b422c7c3d2ca
Marissa L <marissal.io@gmail.com>
2018-03-17 12:53:46 -0400

    m-r a1

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -9,9 +9,31 @@
  * Do not assume the ranges are in order
  */
 
+var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]];
+mergeRanges(times);
 
 function mergeRanges(array) {
+  const res = [];
 
+  for(let i = 0; i < array.length; i++) {
+    let reset = 0;
+    let low = array[i][0];
+    let high = array[i][1];
+    res.forEach((el, i) => {
+      if(low >= el[0] && low <= el[1]) {
+        if(high > el[1]) el[1] = high;
+        reset = 1;
+      }
+      else if (high >= el[0] && high <= el[1]) {
+        if (low < el[0]) el[0] = low;
+        reset = 1;
+      } 
+    });
+
+    if(!reset) res.push(array[i]);
+  }
+
+  return res;
 }
 
 module.exports = mergeRanges;
