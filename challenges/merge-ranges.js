47c4cb00e099184497d555e25441a2e722aaa14e
WuPenghe <wupenghe@MacBook-Pro-3.fios-router.home>
2018-03-17 12:44:50 -0400

    mergerange

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,33 +11,19 @@
 
 
  function mergeRanges(arr) {
+   arr.sort( (a,b) => {
+     return a[0] - b[0];
+   })
    const result = [];
    result.push(arr[0]);
+   let last = arr[0];
    for (let i=1; i<arr.length; i++) {
-     let contains = false;
-     for (let j=1; j<result.length; j++) {
-       if (arr[i][0] >= result[j][0] && arr[i][1] <= result[j][1]) {
-         contains = true;
-         break;
-       }
-       if (arr[i][0] <= result[j][0] && arr[i][1] >= result[j][1]) {
-         contains = true;
-         result[j][0] = arr[i][0];
-         result[j][1] = arr[i][1];
-         break;
-       }
-       if (arr[i][0] < result[j][0] && arr[i][1] < result[j][1] && arr[i][1] >= result[j][0]) {
-         contains = true;
-         result[j][0] = arr[i][0];
-         break;
-       }
-       if (arr[i][0] > result[j][0] && arr[i][1] > result[j][1] && arr[i][0] <= result[j][1]) {
-         contains = true;
-         result[j][1] = arr[i][1];
-         break;
-       }
+     if (arr[i][0] > last[1]) {
+       last = arr[i];
+       result.push(arr[i]);
+     } else if (arr[i][1] > last[1]) {
+       last[1] = arr[i][1];
      }
-     if (!contains) result.push(arr[i]);
    }
    return result;
  }
