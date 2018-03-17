7e6337add2d0f0b2f2838ec62e2d24653d6acf9e
sarahheacock <seheacock@bellsouth.net>
2018-03-17 09:42:56 -0400

    first attempt

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,26 @@
 
 
 function mergeRanges(array) {
+  array.sort((a, b) => {
+    return a[0] - b[0];
+  })
 
+  const result = [array[0]];
+
+  for(let i = 1; i < array.length; i++){
+    const prev = result[result.length - 1];
+    const curr = array[i];
+    if(prev[1] >= curr[0]){
+      prev[1] = Math.max(curr[1], prev[1]);
+    }
+    else {
+      result.push(curr);
+    }
+  }
+
+  return result;
 }
+
+console.log(mergeRanges([[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]))
 
 module.exports = mergeRanges;
