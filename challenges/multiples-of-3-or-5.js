bc491856bbf4ce6189ed213bdc73d16fab16f861
sarahheacock <seheacock@bellsouth.net>
2018-03-20 10:18:31 -0400

    hjk

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -4,12 +4,40 @@
 
 // write a function that will find the sum of all the multiples of 3 or 5
 // below 1000 and return that sum.
-
-function sumMultiples3Or5Below1000() {
+function test() {
   let sum = 0;
+  const num = 10;
+
+  for(let i = 1; i < num; i++){
+    if(i%3 === 0 || i%5 === 0){
+      sum += i;
+    }
+  }
 
   return sum;
 }
+
+function calc(num, limit){
+  let start = limit;
+  while(start%num){
+    start--;
+  }
+  return (start + num) * Math.floor(start / num) / 2;
+}
+
+function sumMultiples3Or5Below1000() {
+  // const num = 10;
+  // // console.log(calc(15, num))
+  // let sum = calc(5, num) + calc(3, num) - calc(15, num);
+  //
+  // return sum;
+  test();
+}
+
+
+
+// console.log(test());
+// console.log(sumMultiples3Or5Below1000())
 
 
 // extension make it dynamic function that takes input x,y,z
