13bd2653e6f1edcdd5df807239f4d2032f98f100
sarahheacock <seheacock@bellsouth.net>
2018-03-21 09:45:48 -0400

    first attempt

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,27 @@
  */
 
 function maxSubarray(arr) {
+  if(arr.length < 4){
+    return;
+  }
 
+  let sub = arr[0];
+  let total = arr.slice(0, 4).reduce((sum, num) => { return sum + num; });
+
+  for(let i = 4; i < arr.length; i++){
+    const add = arr[i];
+    const newNum = total + add - sub;
+
+    if(newNum >= total){
+      total = newNum;
+      sub = arr[i - 3];
+      console.log(arr[i], total, sub);
+    }
+  }
+
+  return total;
 }
+
+console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));
 
 module.exports = maxSubarray;
