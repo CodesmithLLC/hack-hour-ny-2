0e3951b4243bb71dc77bc5a8944507bd6f2d7b29
WuPenghe <wupenghe@MacBook-Pro-3.fios-router.home>
2018-03-21 23:37:21 -0400

    max-subarr

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -12,10 +12,11 @@ function maxSubarray(arr) {
   for (let i=0; i<arr.length; i++) {
     let sum = 0;
     for (let j=i; j<arr.length; j++) {
-      sum +=
+      sum += arr[j];
+      if (sum > max) max=sum;
     }
   }
-
+  return max;
 }
 
 module.exports = maxSubarray;
