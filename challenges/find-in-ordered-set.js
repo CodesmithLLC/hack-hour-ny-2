c14c674f4662a7287de598550e634ab9a32cd17f
Marissa L <marissal.io@gmail.com>
2018-03-26 09:29:29 -0400

    a1 fios

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -8,10 +8,17 @@ findInOrderedSet(nums, 4);  -> true
 findInOrderedSet(nums, 2);  -> false
 
  */
+// var nums = [1, 4, 6, 7, 9, 17, 45]
+// console.log(findInOrderedSet(nums, 3));
 
-
+//binary search
 function findInOrderedSet(arr, target) {
-
+  if(arr[0] === target) return true;
+  if(arr.length === 1 && arr[0] !== target) return false;
+  
+  let mid = Math.floor(arr.length/2);
+  if(target < arr[mid]) return findInOrderedSet(arr.slice(0, mid), target);
+  return findInOrderedSet(arr.slice(mid), target);
 }
 
 
