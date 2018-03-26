ce93dd3b0d110333c3f74226c6ec8dac5cdb1529
jmw1493 <weissjmw@gmail.com>
2018-03-26 09:35:41 -0400

    ... ordered set improved

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -10,13 +10,23 @@ findInOrderedSet(nums, 2);  -> false
  */
 
 function findInOrderedSet(arr, target) {
-  let left = 0;
-  let right = arr.length - 1;
-  while(left <= right) {
-    if (arr[left++] === target || arr[right--] === target) return true;
-  }
+  if (arr.length === 1 && arr[0] !== target) return false;
+  let midInd = Math.floor(arr.length / 2);
+  if (target > arr[midInd]) return findInOrderedSet(arr.slice(midInd, arr.length), target);
+  if (target < arr[midInd]) return findInOrderedSet(arr.slice(0, midInd), target);
+  if (target === arr[midInd]) return true;
   return false;
 }
+
+
+// function findInOrderedSet(arr, target) {
+//   let left = 0;
+//   let right = arr.length - 1;
+//   while(left <= right) {
+//     if (arr[left++] === target || arr[right--] === target) return true;
+//   }
+//   return false;
+// }
 
 // function findInOrderedSet(arr, target) {
 //   const str = arr.join('');
