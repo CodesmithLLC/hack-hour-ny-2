9c56aac1d8e418eb5861a71562f56f828744d093
sarahheacock <seheacock@bellsouth.net>
2018-03-27 09:47:57 -0400

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,25 @@
 
 
 function insertionSort(array) {
+  let result = [array[0]];
 
+  for(let i = 1; i < arr.length; i++){
+    let j = result.length - 1;
+    result.push(null);
+
+    while(j > -1 && result[j] > arr[i]){
+      result[j + 1] = result[j]
+      j--;
+    }
+
+    result[j + 1] = arr[i];
+  }
+
+  array = result;
+  return result;
 }
+
+// const arr = [6, 5, 3, 1, 8, 7, 2, 4];
+// console.log(insertionSort(arr));
 
 module.exports = insertionSort;
