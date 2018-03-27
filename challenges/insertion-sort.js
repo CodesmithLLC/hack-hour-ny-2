4970484b9cd12dd28fdd90b986709800e90ac749
Kyung Lee <olive27k@gmail.com>
2018-03-27 09:40:21 -0400

    find

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,15 @@
 
 
 function insertionSort(array) {
-
+  let buffer, j;
+  for (let i = 1; i < array.length; i++) {
+    j = i - 1;
+    while (array[j] > array[j + 1]) {
+      [array[j], array[j + 1]] = [array[j + 1], array[j]]
+      j--;
+    }
+  }
+  return array;
 }
 
 module.exports = insertionSort;
