38195206bafc0686b59f0282109aebcb2497d1fd
jonathan coe <jonathancoe@jonathans-MacBook-Pro-2.local>
2018-03-27 09:37:30 -0400

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,20 @@
 
 
 function insertionSort(array) {
-
+	let i = 1;
+	while(i < array.length){
+		let x = array[i]
+		let j = i - 1;
+		while(j >= 0 && array[j] > x){
+			array[j+1] = array[j];
+			j--;
+		}
+		array[j+1] = x
+		i++
+	}
+	return array;
 }
+
+console.log(insertionSort([6,1,4,9,3,56,4,3]))
 
 module.exports = insertionSort;
