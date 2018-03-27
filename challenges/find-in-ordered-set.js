b6d08e7e6c03dab35f48d180739956afc8efe399
sarahheacock <seheacock@bellsouth.net>
2018-03-27 09:33:12 -0400

    find in order

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,37 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
+  if(arr[0] > target || arr[arr.length - 1] < target){
+    return false;
+  }
 
+  const top = arr.length - 1;
+  const bottom = 0;
+  let i = Math.ceil((top) / 2);
+
+  while(arr[i] !== target){
+    if(arr[i - 1] < target && arr[i] > target){
+      return false;
+    }
+
+    if(arr[i] > target){
+      i -= Math.ceil((i - bottom) / 2);
+    }
+    else {
+      i += Math.ceil((top - i) / 2);
+    }
+  }
+
+  return true;
 }
 
+// var nums = [1, 4, 6, 7, 9, 17, 45]
+// console.log("true", findInOrderedSet(nums, 4));  // -> true
+// console.log("false", findInOrderedSet(nums, 2));  // -> false
+// console.log("true", findInOrderedSet(nums, 1));
+// console.log("true", findInOrderedSet(nums, 45));
+// console.log("true", findInOrderedSet(nums, 17));
+// console.log("false", findInOrderedSet(nums, 0));
+// console.log("false", findInOrderedSet(nums, 46));
 
 module.exports = findInOrderedSet;
