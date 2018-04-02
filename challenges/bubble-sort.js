79eaac87665ffef5ec9e49c1c7bb5155025a6846
Eduardo Maíllo Conesa <eduardomaillo@gmail.com>
2018-04-02 09:49:13 -0400

    First commit

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,16 @@
 
 
 function bubbleSort(array) {
-
+  for (let i = 0; i < array.length; i += 1) {
+    for (j = 0; j < array.length - i; j += 1) {  
+      if (array[j + 1] <= array[j]) { 
+        temp = array[j];
+        array[j] = array[j + 1]
+        array[j + 1] = temp 
+      }
+    }
+  }
+  return array
 }
 
 module.exports = bubbleSort;
