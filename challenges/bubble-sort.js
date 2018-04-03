323eccce0fd431ea9379dfe15ccb11e3229c4136
Marissa L <marissal.io@gmail.com>
2018-04-03 09:18:28 -0400

    bubblesort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -2,10 +2,23 @@
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
-
+const arr = [1, 3, 2, 5, 0];
+console.log(bubbleSort(arr));
 
 function bubbleSort(array) {
-
+  let swap = true;
+  while (swap) {
+    swap = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        swap = true;
+        const temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
