3c5993fa60aa56318bc65c2e9e7e1aeb5cb4e329
sarahheacock <seheacock@bellsouth.net>
2018-04-03 09:34:13 -0400

    bubble phonebook

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -18,20 +18,27 @@
 *
 * How efficient can you make this?
 
-* Part 2: 
+* Part 2:
 * Why are we storing names and phone numbers in an array?
-* develop a function that takes in the poorly constructed jazbook and returns a proper phonebook 
+* develop a function that takes in the poorly constructed jazbook and returns a proper phonebook
 * complete with methods to add new names and look up and remove existing entries
 */
 
 //  return the number associated with the name in the jazbook
 function findName(jazbook, name) {
-
+  for(let i = 0; i < jazbook.length; i++){
+    if(jazbook[i][0] === name){
+      return jazbook[i][1];
+    }
+  }
+  return false;
 }
 
 // return an object literal representing the jazbook
 function makePhoneBookObject(jazbook){
-
+  return jazbook.reduce((obj, arr) => {
+    obj[arr[0]] = arr[1];
+  }, {});
 }
 
 const objectToExport = {
