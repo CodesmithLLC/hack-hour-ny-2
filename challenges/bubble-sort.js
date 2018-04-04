b83277b50a4fa9035b000f8cc914e3a4558d89c8
Kyung Lee <olive27k@gmail.com>
2018-04-04 09:40:58 -0400

    phonebook

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,22 @@
 
 
 function bubbleSort(array) {
-
+  function bubbleSort(array) {
+    let isSorted = false;
+    let buffer;
+    while (!isSorted) {
+      isSorted = true;
+      for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+          buffer = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = buffer;
+          isSorted = false;
+        }
+      }
+    }
+    return array;
+  }
 }
 
 module.exports = bubbleSort;
