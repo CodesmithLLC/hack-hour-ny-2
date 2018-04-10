acdaa45f6df242c4a0609e6421dc952b7a120858
Marissa L <marissal.io@gmail.com>
2018-04-10 09:40:16 -0400

    knight jump

--- challenges/knight-jumps.js
+++ challenges/knight-jumps.js
@@ -8,10 +8,31 @@
 // For example: if str is "(4 5)" then your program should output 8 because the knight can move to 8 different spaces
 // from position x = 4 and y = 5.
 //  example input:
-// var str = "(4 5)"
+// const str1 = "(0 0)";
+
+function validMove(x, y) {
+  const validX = x >= 0 && x <= 8;
+  const validY = y >= 0 && y <= 8;
+  return validX && validY ? 1 : 0;
+}
 
 function knightjumps(str) {
+  const x = Number(str.substring(1, 3));
+  const y = Number(str.substring(3, 4));
 
+  const xMove = [1, 2];
+  const yMove = [2, 1];
+
+  let ways = 0;
+
+  for (let i = 0; i < xMove.length; i += 1) {
+    ways += validMove(x + xMove[i], y + yMove[i]);
+    ways += validMove(x + xMove[i], y + (-1 * yMove[i]));
+    ways += validMove(x + (-1 * xMove[i]), y + yMove[i]);
+    ways += validMove(x + (-1 * xMove[i]), y + (-1 * yMove[i]));
+  }
+
+  return ways;
 }
 
 module.exports = knightjumps;
