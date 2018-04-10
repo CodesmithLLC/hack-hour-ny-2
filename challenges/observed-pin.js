e88338718645a6e7cbaf280e08e355be2d3b56d3
sarahheacock <seheacock@bellsouth.net>
2018-04-10 10:34:09 -0400

    observed pins

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -1,7 +1,7 @@
 /*
-Alright, detective, one of our colleagues successfully observed our target person, Robby the robber. 
-We followed him to a secret warehouse, where we assume to find all the stolen stuff. The door to 
-this warehouse is secured by an electronic combination lock. Unfortunately our spy isn't sure about the 
+Alright, detective, one of our colleagues successfully observed our target person, Robby the robber.
+We followed him to a secret warehouse, where we assume to find all the stolen stuff. The door to
+this warehouse is secured by an electronic combination lock. Unfortunately our spy isn't sure about the
 PIN he saw, when Robby entered it.
 
 The keypad has the following layout:
@@ -15,18 +15,18 @@ The keypad has the following layout:
 └───┼───┼───┘
     │ 0 │
     └───┘
-He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually 
-be another adjacent digit (horizontally or vertically, but not diagonally). E.g. instead of the 1 it 
+He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually
+be another adjacent digit (horizontally or vertically, but not diagonally). E.g. instead of the 1 it
 could also be the 2 or 4. And instead of the 5 it could also be the 2, 4, 6 or 8.
 
-He also mentioned, he knows this kind of locks. You can enter an unlimited amount of wrong PINs, they 
+He also mentioned, he knows this kind of locks. You can enter an unlimited amount of wrong PINs, they
 never finally lock the system or sound the alarm. That's why we can try out all possible (*) variations.
 
 * possible in sense of: the observed PIN itself and all variations considering the adjacent digits
 
-Can you help us to find all those variations? It would be nice to have a function, that returns an array 
-of all variations for an observed PIN with a length of 1 to 8 digits. We could name the function getPINs. 
-But please note that all PINs, the observed one and also the results, must be strings, because of 
+Can you help us to find all those variations? It would be nice to have a function, that returns an array
+of all variations for an observed PIN with a length of 1 to 8 digits. We could name the function getPINs.
+But please note that all PINs, the observed one and also the results, must be strings, because of
 potentially leading '0's. Don't worry about the order of the array.
 
 Detective, we count on you!
@@ -39,12 +39,56 @@ expectations = {
 
 */
 
+function comb(arr, result){
+  // const done = arr.reduce((ways) => { return done && ways.length === 0; }, true)
+  // if(done) return result;
+  if(arr.length < 1) {
+    return [];
+  }
+  if(arr.length < 2) {
+    return arr[0]
+  }
+  if(arr.length > 2){
+    arr[1] = comb(arr.slice(1), []);
+  }
 
+  for(let i = 0; i < arr[0].length; i++){
+    for(let j = 0; j < arr[1].length; j++){
+      result.push(arr[0][i] + arr[1][j]);
+    }
+  }
 
-
-function getPINs(observed) {
-
+  return result;
 }
 
 
+function getPINs(observed) {
+  const poss = [
+    ["0", "8"],
+    ["1", "2", "4"],
+    ["1", "2", "3", "5"],
+    ["2", "3", "6"],
+    ["1", "4", "5", "7"],
+    ["2", "4", "5", "6", "8"],
+    ["3", "5", "6", "9"],
+    ["4", "7", "8"],
+    ["5", "7", "8", "9", "0"],
+    ["6", "8", "9"]
+  ];
+
+  // split each of observed into possible
+  const temp = observed.split('').map((num) => {
+    const index = parseInt(num, 10);
+    return poss[index];
+  })
+
+  const combArr = comb(temp, []);
+  return Object.keys(combArr.reduce((obj, nums) => {
+    obj[nums] = true;
+    return obj;
+  }, {}));
+}
+
+// console.log(getPINs("369").length);
+// console.log(["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"].length)
 module.exports = getPINs
