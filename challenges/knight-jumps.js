ccbb7641801052ad5947c0a3b27c1ae4bd61f2bb
Kyung Lee <olive27k@gmail.com>
2018-04-10 10:55:58 -0400

    knight pascal2

--- challenges/knight-jumps.js
+++ challenges/knight-jumps.js
@@ -40,40 +40,40 @@ function knightjumps(str) {
 }
 
 
-// check using the position of the square instead of examining the possible moves
-function knightjumps(str) {
-  var x = 1 * str[1]; // another way to parse integers
-  var y = 1 * str[3];
+// // check using the position of the square instead of examining the possible moves
+// function knightjumps(str) {
+//   var x = 1 * str[1]; // another way to parse integers
+//   var y = 1 * str[3];
 
-  var spaces = 0;
-  if (x - 1 >= 1) {
-    if (y - 2 > 0) spaces++;
-    if (y + 2 < 9) spaces++;
-  }
-  if (x - 2 >= 1) {
-    if (y - 1 > 0) spaces++;
-    if (y + 1 < 9) spaces++;
-  }
-  if (x + 1 <= 8) {
-    if (y - 2 > 0) spaces++;
-    if (y + 2 < 9) spaces++;
-  }
-  if (x + 2 <= 8) {
-    if (y - 1 > 0) spaces++;
-    if (y + 1 < 9) spaces++;
-  }
-  return spaces;
-}
+//   var spaces = 0;
+//   if (x - 1 >= 1) {
+//     if (y - 2 > 0) spaces++;
+//     if (y + 2 < 9) spaces++;
+//   }
+//   if (x - 2 >= 1) {
+//     if (y - 1 > 0) spaces++;
+//     if (y + 1 < 9) spaces++;
+//   }
+//   if (x + 1 <= 8) {
+//     if (y - 2 > 0) spaces++;
+//     if (y + 2 < 9) spaces++;
+//   }
+//   if (x + 2 <= 8) {
+//     if (y - 1 > 0) spaces++;
+//     if (y + 1 < 9) spaces++;
+//   }
+//   return spaces;
+// }
 
-// math it up
-function knightjumps(str) {
-  var x = reflect(+str[1]); // yet another way to parse integers, then reflect the coordinate
-  var y = reflect(+str[3]);
-  var x_distance = Math.max(3 - x, 0);
-  var y_distance = Math.max(3 - y, 0);
-  return 8 - distanceToValue(x_distance + y_distance);
-}
-function distanceToValue(d) { return d < 2 ? 2 * d : d + 2; }
-function reflect(n) { return n > 4 ? 9 - n : n; }
+// // math it up
+// function knightjumps(str) {
+//   var x = reflect(+str[1]); // yet another way to parse integers, then reflect the coordinate
+//   var y = reflect(+str[3]);
+//   var x_distance = Math.max(3 - x, 0);
+//   var y_distance = Math.max(3 - y, 0);
+//   return 8 - distanceToValue(x_distance + y_distance);
+// }
+// function distanceToValue(d) { return d < 2 ? 2 * d : d + 2; }
+// function reflect(n) { return n > 4 ? 9 - n : n; }
 
 module.exports = knightjumps;
--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -38,32 +38,32 @@
 // }
 
 // CSmith Erik
-// Recursive
-function pascalTriangle(n, triangle) {
-  if (!n || n < 1) return [];
+// // Recursive
+// function pascalTriangle(n, triangle) {
+//   if (!n || n < 1) return [];
 
-  // this line handles the initial call which may not pass in a triangle
-  triangle = triangle || [[1]];
+//   // this line handles the initial call which may not pass in a triangle
+//   triangle = triangle || [[1]];
 
-  // base case : n is 1. Return whatever our triangle is at that point.
-  if (n < 2) return triangle;
+//   // base case : n is 1. Return whatever our triangle is at that point.
+//   if (n < 2) return triangle;
 
-  // recursive case:
-  var row = [1];
-  var prevRow = triangle[triangle.length - 1];
+//   // recursive case:
+//   var row = [1];
+//   var prevRow = triangle[triangle.length - 1];
 
-  for (var i = 1; i < prevRow.length; i++) {
-    row.push(prevRow[i] + prevRow[i - 1]);
-  }
+//   for (var i = 1; i < prevRow.length; i++) {
+//     row.push(prevRow[i] + prevRow[i - 1]);
+//   }
 
-  row.push(1);
-  triangle.push(row);
+//   row.push(1);
+//   triangle.push(row);
 
-  // at the end of the recursive case, make the recursive call with n - 1
-  // keep in mind, n is only used as a counter. we build from triangle, which
-  // will start from [[1]]
-  return pascalTriangle(n - 1, triangle);
-}
+//   // at the end of the recursive case, make the recursive call with n - 1
+//   // keep in mind, n is only used as a counter. we build from triangle, which
+//   // will start from [[1]]
+//   return pascalTriangle(n - 1, triangle);
+// }
 
 // Iterative
 function pascalTriangle(numRows) {
