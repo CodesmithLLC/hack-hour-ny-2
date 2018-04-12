8bc6af10fdec01baf0523cf65f8247bdb5908f75
ralph salazar <ralph.slzr@gmail.com>
2018-04-12 09:48:26 -0400

    first attempt in a while

--- challenges/missing-number.js
+++ challenges/missing-number.js
@@ -1,6 +1,6 @@
-/* 
-A zero-indexed array A consisting of N different integers is given. 
-The array contains integers in the range [1..(N + 1)], which means 
+/*
+A zero-indexed array A consisting of N different integers is given.
+The array contains integers in the range [1..(N + 1)], which means
 that exactly one element is missing.Your goal is to find that missing element.
 Write a function that, given an array A, returns the value of the missing element.
 
@@ -21,11 +21,25 @@ Assume that:
 expected worst-case time complexity is O(N);
 expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
 
-Challange: 
+Challange:
   ** cannot use additional storage, variables are okay not any TYPE of object
   ** keep in mind time complexity
 */
-function missingNum(Array) {
+function missingNum(array) {
+  // sort array in place and and check if previous number is exactly one less than previous number
+  // orrrrrrr find what the total of numbers should be and subtract the numbers - the result is either 0 or the missing number
+  // 5+4+3+2+1 = 15, 2+3+1+5 = 11, 15 - 11 = 4, the missing number BUT this is the worst case time & space complexity...
+  let largest = array[0];
+  let arrTotal = array.reduce((acc, num) => {
+    if (num > largest) largest = num;
+    return acc + num;
+  }, 0);
+
+  let total = 0;
+  for (let i = 1; i <= largest; i++) {
+    total += i;
+  }
+  return total - arrTotal;
 }
 
 module.exports = missingNum;
