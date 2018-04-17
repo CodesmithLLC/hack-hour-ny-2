18482b0affff16f979461cae44432fea51fb7dc5
sarahheacock <seheacock@bellsouth.net>
2018-04-17 09:45:41 -0400

    hjk

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -9,8 +9,23 @@
   solveKnapsack(items, 5); // returns 9 (from items[1] and items[2])
 */
 
-function solveKnapsack(items, weightAvailable) {
+function solveKnapsack(items, weightAvailable, result=0) {
+  // console.log(result);
 
+  if(!items.length || !weightAvailable){
+    return result;
+  }
+
+  if(items[0].weight <= weightAvailable){
+    return Math.max(solveKnapsack(items.slice(1), weightAvailable, result), solveKnapsack(items.slice(1), weightAvailable - items[0].weight, result + items[0].value));
+  }
+  else {
+    return solveKnapsack(items.slice(1), weightAvailable, result);
+  }
 };
+
+const items = [{weight: 1, value : 3}, {weight: 2, value : 4}, {weight: 3, value : 5}];
+console.log(solveKnapsack(items, 3));
+console.log(solveKnapsack(items, 5));
 
 module.exports = solveKnapsack;
