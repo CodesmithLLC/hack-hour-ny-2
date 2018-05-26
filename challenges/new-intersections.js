f3a3f6dd664df460e3406335f25c51bc58397769
jonathan coe <jonathancoe@jonathans-MBP-2.EAP1750H1>
2018-05-26 10:20:43 -0400

    n i

--- challenges/new-intersections.js
+++ challenges/new-intersections.js
@@ -14,11 +14,42 @@
  *
  * 	 - x and y will contain the same number of elements
  *   - if a new point is bounded by old points and lands on an old point, then count it
- * 	 stuff
+ * 	
  */
 
-function newIntersections(x, y){
- 
+function makeObj(arr1, arr2){
+	return arr1.reduce((acc, cur, i) => {
+	 	if (!acc[cur]) acc[cur] = { min: arr2[i], max: arr2[i]}
+
+	 	acc[cur].max = Math.max(acc[cur].max, arr2[i])
+	 	acc[cur].min = Math.min(acc[cur].min, arr2[i])
+
+	 	return acc;
+	}, {})
 }
+
+function newIntersections(x, y){
+
+	const xObj = makeObj(x, y)
+	const yObj = makeObj(y, x)
+
+	console.log("xObj : ")
+	console.log(xObj)
+	console.log('----------------------------------')
+	console.log("yObj : ")
+	console.log(yObj)
+
+	let intersections = 0;
+	for (let xCoord in yObj){
+		for (let yCoord in xObj){
+			if ( xObj[yCoord].min < xCoord && xCoord < xObj[yCoord].max &&
+					 yObj[xCoord].min < yCoord && yCoord < yObj[xCoord].max) 
+				intersections++
+		}
+	}
+	return intersections
+}
+
+console.log(newIntersections([0,1,1,],[1,2,0,1]))
 
 module.exports = newIntersections;
