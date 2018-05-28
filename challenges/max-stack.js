286d3c8107294d306add8c10b9aaad891fdda43e
jonathan coe <jonathancoe@jonathans-MBP-2.fios-router.home>
2018-05-27 20:47:52 -0400

    max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,53 +6,35 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-// function Stack() {
-//   let stack = [];
-//   this.push = (e) => {
-//   	stack = stack.concat(e)
-//   	return stack.length;
-//   };
-//   this.pop = () => {
-//     let popped = stack.splice(-1);
-//   	return popped[0];
-//   }
-//   this.getMax = () => {
-//   	let max = stack[0];
-//   	for (let i = 1; i < stack.length; i++){
-//   		if (stack[i] > max) max = stack[i];
-//   	}
-//   	return max;
-//   }
-// }
 
-function Stack() {
-
-  this.stack = {};
-  this.index = 0;
-  this.push = (e) => { 
-    this.stack[this.index] = e;
-    
-    return ++this.index;
-
-  }
-
-  this.pop = () => {
-    if (index === 0) return undefined;
-    this.popped = this.stack[this.index-1];
-    delete this.stack[this.index-1];
-    this.index--;
-    return this.popped;
-  }
-
-  this.getMax = () => {
-    this.max = this.stack[0];
-    for (let i = 1; i < this.index; i++){
-      if (this.stack[i] > this.max) this.max = this.stack[i];
-    }
-    return this.max
-  }
-
+function Stack(){
+  this.stack = [];
+  this.maxStack = [];
 }
+
+Stack.prototype.push = function(val){
+  this.stack.push(val)
+  let max;
+
+  if (!this.maxStack.length || val > this.maxStack[this.maxStack.length - 1]){
+    max = val;
+  } else {
+    max = this.maxStack[this.maxStack.length - 1];
+  }
+
+  return this.maxStack.push(max)
+}
+
+Stack.prototype.pop = function(){
+  this.maxStack.pop()
+  return this.stack.pop()
+}
+
+Stack.prototype.getMax = function() {
+  return this.maxStack[this.maxStack.length - 1];
+}
+
+
 const STACK = new Stack();
 
 console.log(STACK.push(0)); 
