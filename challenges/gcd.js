731eb9d7904bc744cc81788ae81e5ab8d54e9711
jonathan coe <jonathancoe@jonathans-MacBook-Pro-2.local>
2018-06-13 13:33:48 -0500

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,15 +7,24 @@
  *
  */
 
-function gcd(a, b) {
-	let divisor = a > b ? b : a;
-	while (divisor > 1){
-		if (a%divisor === 0 && b%divisor === 0)
-			return divisor;
-		divisor--;
-	}
-	return 1;
-}
+const gcd = (a, b, div = a > b ? b : a) => (!(a % div) && !(b % div)) ? div : gcd(a, b, --div);
+
+
+// function gcd(a, b) {
+// 	let divisor = a > b ? b : a;
+// 	while (divisor > 1){
+// 		if (a%divisor === 0 && b%divisor === 0)
+// 			return divisor;
+// 		divisor--;
+// 	}
+// 	return 1;
+// }
+
+// Principle
+// The GCD of 2 numbers doesn't change if the larger 
+// number is replaced by its difference with the lower number
+
+const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
 
 console.log(gcd(10,8))
 console.log(gcd(10,9))
