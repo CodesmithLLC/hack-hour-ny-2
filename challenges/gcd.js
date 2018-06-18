6bdcab21a4dd899e8ed45de48f778cedfdf0aba5
jonathan coe <jonathancoe@jonathans-MacBook-Pro-2.local>
2018-06-18 16:44:10 -0400

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,24 +7,43 @@
  *
  */
 
-const gcd = (a, b, div = a > b ? b : a) => (!(a % div) && !(b % div)) ? div : gcd(a, b, --div);
+
+const gcd = (a, b) => {
+	let divisor = a > b ? b : a;
+	while (divisor > 1){
+		if (a%divisor === 0 && b%divisor === 0)
+			return divisor;
+		divisor--;
+	}
+	return 1;
+}
 
 
-// function gcd(a, b) {
-// 	let divisor = a > b ? b : a;
-// 	while (divisor > 1){
-// 		if (a%divisor === 0 && b%divisor === 0)
-// 			return divisor;
-// 		divisor--;
-// 	}
-// 	return 1;
+
+
+
+// const gcd = (a, b, div = a > b ? b : a) => {
+// 	if (!(a % div) && !(b % div)) return div;
+// 	else return gcd(a, b, --div);
 // }
 
-// Principle
-// The GCD of 2 numbers doesn't change if the larger 
-// number is replaced by its difference with the lower number
 
-const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
+
+
+
+// const gcd = (a, b, div = a > b ? b : a) => (!(a % div) && !(b % div)) ? div : gcd(a, b, --div);
+
+
+
+// gcd (252, 105) = 21
+// 			252 % 105 = 42
+// gcd (105, 42) = 21
+// 			105 % 42  = 21
+// gcd (42, 21) = 21
+// 			42 % 21 = 0
+
+
+// const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
 
 console.log(gcd(10,8))
 console.log(gcd(10,9))
