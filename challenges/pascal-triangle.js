fd809336a700232bf8e054c993c038f83979f590
jonathan coe <jonathancoe@jonathans-MBP-2.fios-router.home>
2018-07-15 23:17:31 -0400

    asdf

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -35,22 +35,19 @@
 function pascalTriangle(numRows, arr = [[1]]) {
   if (numRows <= 0) return [];
   if (numRows === 1) return arr;
+
   const row = [1]
   const prev = arr[arr.length-1]
-  
-  if (prev.length === 1) row.push(1)
-  else {
-    for (let i = 0; i < prev.length-1; i++){
 
-      row.push(prev[i] + prev[i+1])
-    }
-    row.push(1)
+  for (let i = 0; i < prev.length-1; i++){
+    row.push(prev[i] + prev[i+1])
   }
 
+  row.push(1)
   arr.push(row)
   return pascalTriangle(numRows-1, arr);
 }
 
-console.log(pascalTriangle(9))
+console.log(pascalTriangle(2))
 
 module.exports = pascalTriangle;
